72e276d9675aab509676068a34607768fc7cae9b
Mandeepsgm <mandeepsg.m@gmail.com>
2022-12-27 22:14:08 -0500

    adding features component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './features.css';
 
-const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+    
+    </div>
+  </div>
+);
 
-export default Features
+export default Features;
